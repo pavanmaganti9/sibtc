1612f75cd086f90dcb5aa8f3d01027f72753903a
PVPavanMaganti <pavan@markitome.com>
2018-04-29 15:43:13 +0530

    modified

--- project/project/boards/sample.py
+++ project/project/boards/sample.py
@@ -1,5 +1,5 @@
 def my_function():
     return "Hello From My Function!"
-    # return{
-    #      "message": "Here is some text"
-    # }
+    return{
+         "message": "Here is some text"
+    }
